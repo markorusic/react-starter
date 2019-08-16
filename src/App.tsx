d9eddd506d7eae5b06f29332edae59c87f7af039
Marko Rusic <markousp5@gmail.com>
2019-08-16 03:37:33 +0200

    eslint/prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react'
 
-const App: React.FC = () => <h1>react-starter</h1>;
+const App: React.FC = () => <h1>react-starter</h1>
 
-export default App;
+export default App
